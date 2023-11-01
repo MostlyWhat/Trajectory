9aa2298b036d46fc0360729a8b05f3e2e3b8d0cf
MostlyWhat <mostlywhatstudios@gmail.com>
2023-11-01 21:40:27 +0700

    Base Template Upload

---
+++ includes/theme.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package   Gantry5
+ * @author    MostlyWhat Systems http://www.mostlywhat.com
+ * @copyright Copyright (C) 2007 - 2022 MostlyWhat Systems
+ * @license   MIT
+ *
+ * http://opensource.org/licenses/MIT
+ */
+
+namespace Gantry\Theme;
+
+use Gantry\Framework\Theme;
+
+class Trajectory extends Theme {}
